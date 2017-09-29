a3d4037e0923ff91b902ba7d0df6881c4783081b
Breno Viana <brenomfviana@gmail.com>
2017-09-28 22:59:05 -0300

    Starts the implementation of ninja behavior.

--- game/scripts/ninja.gd
+++ game/scripts/ninja.gd
@@ -10,19 +10,27 @@ var run_speed = 100
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	set_process(true)
-	pass
+	set_fixed_process(true)
 
-func _process(delta):
+func _fixed_process(delta):
 	# Called every frame
 	var right = 0
 	var left  = 0
+	var up    = 0
+	var down  = 0
 	# Check if the right key was pressed
 	if Input.is_action_pressed("ui_right"):
 		right = 1
 	# Check if the left key was pressed
 	if Input.is_action_pressed("ui_left"):
 		left = -1
+	# Check if the right key was pressed
+	if Input.is_action_pressed("ui_up"):
+		up = -1
+	# Check if the left key was pressed
+	if Input.is_action_pressed("ui_down"):
+		down = 1
 	# Update position
-	set_pos(get_pos() + Vector2(speed, 0) * delta * (right + left))
-	pass
+	var body = get_node("KinematicBody2D")
+	body.move_to(get_pos() + Vector2(speed, 0) * delta * (right + left)
+		+ Vector2(0, speed) * delta * (up + down))
