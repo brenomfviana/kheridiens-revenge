408a391dd0fde6cbcae4d57d4d92d203165c2bd3
Breno Viana <brenomfviana@gmail.com>
2017-10-08 12:22:37 -0300

    Updates main menu and create intructions and credits scenes.

--- game/scripts/menu.gd
+++ game/scripts/menu.gd
@@ -10,6 +10,12 @@ func _process(delta):
 	# Start game
 	if (get_node("panel/start_game").is_pressed()):
 		get_tree().change_scene("res://scenes/phases/phase_one.tscn")
+	# Instructions
+	if (get_node("panel/instructions").is_pressed()):
+		get_tree().change_scene("res://scenes/instructions.tscn")
+	# Credits
+	if (get_node("panel/credits").is_pressed()):
+		get_tree().change_scene("res://scenes/credits.tscn")
 	# Quit game
 	if (get_node("panel/quit_game").is_pressed()):
 		get_tree().quit()
